e27fdc5e522ebc80b0d43c05f6740cd7dda32e4e
David Schneider <post.davidschneider@gmail.com>
2024-07-23 20:00:27 +0200

    specify missing plugin classes

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <file_picker/file_picker_linux.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) file_picker_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FilePickerLinux");
+  file_picker_linux_register_with_registrar(file_picker_registrar);
 }
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <file_picker/file_picker_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FilePickerWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FilePickerWindows"));
 }
